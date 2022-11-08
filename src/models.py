bd7bff74d76e24a310c8f603e9809e6137d027e0
DiegoLigtenberg <diegoligtenberg@gmail.com>
2022-11-08 10:45:49 +0100

    ttl update

--- src/models.py
+++ src/models.py
@@ -92,7 +92,7 @@ class SoundToText():
     def wav2vec2(self,size):
         pass
 
-    # @st.cache(ttl=10)
+    @st.cache(ttl=10)
     def whisper(self):
         # download youtube url
         self.timestr = time.strftime("%Y%m%d-%H%M%S")
