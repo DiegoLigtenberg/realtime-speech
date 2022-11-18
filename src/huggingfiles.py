df710a34c7c44345570875d098ab5bebfb8cc6a3
DiegoLigtenberg <48967029+DiegoLigtenberg@users.noreply.github.com>
2022-11-18 13:07:04 +0100

    azure repo

---
+++ src/huggingfiles.py
@@ -0,0 +1,7 @@
+from collections import OrderedDict 
+from transformers import TRANSFORMERS_CACHE
+ 
+{"url": "https://github.com/huggingface/transformers/issues/861"}
+
+from transformers import file_utils
+print(file_utils.default_cache_path)
