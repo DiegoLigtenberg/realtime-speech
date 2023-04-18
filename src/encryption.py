5639711a57007822a396fe6d2c9ecaad96dfc49f
DiegoLigtenberg <diegoligtenberg@gmail.com>
2023-04-19 00:46:55 +0200

    finished app

--- src/encryption.py
+++ src/encryption.py
@@ -4,10 +4,3 @@ def caesar_decrypt(text, shift):
         decrypted_char = chr((ord(char) - shift) % 128)
         decrypted_text += decrypted_char
     return decrypted_text
-
-# check https://dashboard.stripe.com/products/prod_NjneJRpeYKWVDR
-#^GOX)?9=9
-#^GOX)9769
-#^GOX);879
-#^GOX)=;87
-#^GOX)98<?
